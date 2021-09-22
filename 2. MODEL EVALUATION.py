6af8cc357222ad4b3c367787b3028f64a6eb1d2a
DrishtiBhagat <drisht@gmail.com>
2021-09-22 12:40:35 +0530

    Update 2. MODEL EVALUATION.py

--- 2. MODEL EVALUATION.py
+++ 2. MODEL EVALUATION.py
@@ -20,7 +20,7 @@ def predict(question1, question2, threshold, model, vocab, data_generator=data_g
     Returns:
         bool: True if the questions are duplicates, False otherwise.
     """
-   # use `nltk` word tokenize function to tokenize
+
     q1 = nltk.word_tokenize(question1)  # tokenize
     q2 = nltk.word_tokenize(question2)  # tokenize
     Q1, Q2 = [], []
@@ -30,17 +30,11 @@ def predict(question1, question2, threshold, model, vocab, data_generator=data_g
     for word in q2:  # encode q2
         # increment by checking the 'word' index in `vocab`
         Q2 += [vocab[word]]
-        
-    # Call the data generator (built in Ex 01) using next()
-    # pass [Q1] & [Q2] as Q1 & Q2 arguments of the data generator. Set batch size as 1
-    # Hint: use `vocab['<PAD>']` for the `pad` argument of the data generator
+
     Q1, Q2 = next(data_generator([Q1], [Q2],1,vocab['<PAD>']))
     # Call the model
     v1, v2 = model((Q1,Q2))
-    # take dot product to compute cos similarity of each pair of entries, v1, v2
-    # don't forget to transpose the second argument
     d = np.dot(v1[0],v2[0].T)
-    # is d greater than the threshold?
     res = d>threshold
       
     if(verbose):
